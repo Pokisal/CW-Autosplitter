b80177b2cd98f65af31b34c06a53aeae3b0843b3
Pokisal <93021798+Pokisal@users.noreply.github.com>
2025-05-02 08:19:04 -0500

    Add files via upload

--- UI/Components/CWAutosplitterComponent.ASLFunctions.cs
+++ UI/Components/CWAutosplitterComponent.ASLFunctions.cs
@@ -46,9 +46,9 @@ namespace CWAutosplitter.UI.Components
         public bool Update()
         {
             OldCutsceneIDString = CutsceneIDString;
-            CutsceneID = TCPFunctions.RequestMemory(0xC809393C, 4);
-            InLoad = TCPFunctions.RequestMemory(0xC8093A38, 1).ElementAt(0) != 0;
-            InCutscene = TCPFunctions.RequestMemory(0xC837336C, 1).ElementAt(0) != 0;
+            CutsceneID = TCPFunctions.RequestMemory(0xC809393C, 4, CutsceneID);
+            InLoad = TCPFunctions.RequestMemory(0xC8093A38, 1, BitConverter.GetBytes(InLoad)).ElementAt(0) != 0;
+            InCutscene = TCPFunctions.RequestMemory(0xC837336C, 1, BitConverter.GetBytes(InCutscene)).ElementAt(0) != 0;
             CutsceneIDString = Encoding.UTF8.GetString(CutsceneID);
             return true;
         }
